8cb5355e4c73344eda787961be47e7b07776a65c
Raphael Ako-Mensah <rapha.mensah@gmail.com>
2019-02-03 15:21:19 +0000

    Update images.py
    Changed main script body to a function which get's called again after it finishes.
    Each search now adds a directory which the images will get saved to. 
    Directories are named after search results

--- images.py
+++ images.py
@@ -4,32 +4,40 @@ from PIL import Image
 from io import BytesIO
 import os
 
-search = input("Search for: ")
-params = {"q": search, "pq": search}
-r = requests.get("http://www.bing.com/images/search", params=params)
 
-soup = BeautifulSoup(r.text, "html.parser")
-links = soup.findAll("a", {"class": "thumb"})
+def start_search():
+    search = input("Search for: ")
+    params = {"q": search, "pq": search}
+    dir_name = search.replace(" ", "_").lower()
 
-# Use below code to save a copy of the parsed HTML page
-f = open("test_results.html", "w+")
-f.write(str(soup))
-f.close()
+    # Creates a folder for scraped images if there isn't one. The title of the directory is the search term
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
 
-# loop through links and extracts href and saves the image file in a directory with part of the href as the title
-for item in links:
-   try:
-       img_obj = requests.get(item.attrs["href"])
-       title = item.attrs["href"].split("/")[-1]
-       print("Downloading image:  " + str(title))
-       print()
-       img = Image.open(BytesIO(img_obj.content))
+    r = requests.get("http://www.bing.com/images/search", params=params)
 
-       # Creates a folder for scraped images if there isn't one
-       if not os.path.isdir("scraped_images"):
-           os.makedirs("scraped_images")
+    soup = BeautifulSoup(r.text, "html.parser")
+    links = soup.findAll("a", {"class": "thumb"})
 
-       img.save("./scraped_images/" + title, img.format)
-   except:
-       print("This image could not be downloaded. Trying next")
-       print()
+    # Use below code to save a copy of the parsed HTML page
+    f = open("test_results.html", "w+")
+    f.write(str(soup))
+    f.close()
+
+    # Loop through links and extracts href and saves the image file in the search directory
+    for item in links:
+        img_obj = requests.get(item.attrs["href"])
+        title = item.attrs["href"].split("/")[-1]
+        print("Title is " + str(title))
+        print()
+        try:
+            img = Image.open(BytesIO(img_obj.content))
+            img.save("./" + dir_name + "/" + title, img.format)
+        except:
+            print("This image could not be downloaded. Trying next")
+            print()
+
+    start_search()
+
+
+start_search()
